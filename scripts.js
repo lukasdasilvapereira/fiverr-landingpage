39ae64c30644f45473fa6e755ce80c2dcab834a1
Lucas Pereira <lucasdasilvapereira021@gmail.com>
2025-03-15 13:06:44 -0300

    .

---
+++ scripts.js
@@ -0,0 +1,22 @@
+const botaoEntrar = document.querySelector('.menu-open')
+
+const botaoSair = document.querySelector('.menu-close')
+
+const items = document.querySelector('.popular-section')
+
+
+function entrar() {
+    botaoSair.style.display = 'block'
+    items.style.display = 'flex'
+    botaoEntrar.style.display = 'none'
+    document.body.classList.toggle("show-mobile-menu")
+}
+
+function sair() {
+    botaoSair.style.display = 'none'
+    items.style.display = 'none'
+    botaoEntrar.style.display = 'block'
+
+    document.body.classList.remove("show-mobile-menu");
+
+}
